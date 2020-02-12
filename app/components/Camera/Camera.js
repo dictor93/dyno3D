55a73b94d08d825276f5e860e664a61c40f78083
Viktor Limishenko <dictor93@gmail.com>
2020-02-13 00:33:33 +0200

    Increased camera max distance

--- app/components/Camera/Camera.js
+++ app/components/Camera/Camera.js
@@ -1,6 +1,6 @@
 const { PerspectiveCamera } = require('three');
 
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000)
 camera.position.setY(300)
 camera.position.setX(300)
 
